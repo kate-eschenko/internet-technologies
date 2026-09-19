eed19aec1faf969ea4350b7d4dcb04c0d6e36efc
Kate Eschenko <kate.eschenko@gmail.com>
2026-09-19 19:46:15 +0300

    lesson 2

---
+++ fiit/src/components/Checkbox.vue
@@ -0,0 +1,40 @@
+<template>
+  <input v-model="check" type="checkbox" @change="(e) => addV(6, e)">{{ v }} //
+  <span :class=" check ? 'checkbox__label--green' : 'checkbox__label--red' ">Wow! ({{ props.way }}) </span>
+  <br/>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from "vue";
+
+const props = defineProps({
+  way: {
+    default: '',
+    type: Number
+  }
+})
+
+const emits = defineEmits(['change'])
+
+const v = ref(0)
+const check = ref(false)
+
+const addV = (p: number, e: Event | null = null) => {
+  v.value += p
+  emits('change', v.value)
+}
+
+</script>
+
+<style scoped lang="scss">
+
+.checkbox__label {
+  &--red {
+    color: red;
+  }
+  &--green {
+    color: green;
+  }
+}
+</style>
